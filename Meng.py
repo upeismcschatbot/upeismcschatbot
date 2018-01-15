3110eaf3d36e2283b7488bdc3636e796aeb8e65e
upeismcschatbot <35311737+upeismcschatbot@users.noreply.github.com>
2018-01-15 14:13:51 -0400

    Add files via upload

---
+++ Meng.py
@@ -0,0 +1,65 @@
+from wit import Wit
+AT = "5Z3CEHTOAGRSHCA5MU7CFWHXSOAPDBM2"
+client = Wit(access_token = AT)
+
+def tableselecter():
+    """ an function of table selecter, choose which table need to be use"""
+    return "table1"
+    
+
+while True: 
+    MT = input("input: ")
+
+    resp = client.message(MT)
+
+    print(resp)
+    print()
+    i = 0
+    while 1:
+        try:
+            print(resp['entities']['pname'][i]['value'])
+            i+=1
+        except:
+            break
+
+    i = 0
+    while 1:
+        try:
+            print(resp['entities']['course'][i]['value'])
+            i+=1
+        except:
+            break
+        
+
+    i = 0
+    while 1:
+        try:
+            print(resp['entities']['intent'][i]['value'])
+            i+=1
+        except:
+            break
+"""
+    i = 0
+    while 1:
+        try:
+            resp['entities']['pname'][i]['value']
+            i+=1
+            intent = "professorname"
+            
+        
+            
+
+    i = 0
+    while 1:
+        try:
+            limit = "course = "+resp['entities']['course'][i]['value']
+            i+=1
+            
+            
+        except:
+            break
+            
+    query = "select " + intent + " from " + tableselecter() + " where " + limit
+"""
+
+   
